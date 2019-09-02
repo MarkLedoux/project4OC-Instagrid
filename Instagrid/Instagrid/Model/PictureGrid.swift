de4a797bf26f1d716bba15d4835535fd0ef53c46
vinceled <mark.ledoux@live.fr>
2019-09-02 11:23:12 +0200

    the design of the app in portrait mode is almost complete, except for the behaviour of the buttons in the view containing them

---
+++ Instagrid/Instagrid/Model/PictureGrid.swift
@@ -0,0 +1,13 @@
+//
+//  PictureGrid.swift
+//  Instagrid
+//
+//  Created by Mark LEDOUX on 28/08/2019.
+//  Copyright © 2019 Mark LEDOUX. All rights reserved.
+//
+
+import Foundation
+
+struct PictureGrid {
+    var isSelected = true
+}
