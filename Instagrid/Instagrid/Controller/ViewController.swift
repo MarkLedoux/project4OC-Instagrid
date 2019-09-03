c0c070095e9ae6320ebbd1a8d99e777918df8fc3
vinceled <mark.ledoux@live.fr>
2019-09-03 10:09:30 +0200

    actions to hide and unhide buttons to make the GridView layouts

--- Instagrid/Instagrid/Controller/ViewController.swift
+++ Instagrid/Instagrid/Controller/ViewController.swift
@@ -10,15 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var image = UIImage(named: "Selected")
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    //MARK: bottoms buttons actions
+    @IBOutlet weak var topRightButton: UIButton!
+    @IBOutlet weak var bottomRightButton: UIButton!
+    @IBOutlet weak var topLeftButton: UIButton!
+    @IBOutlet weak var bottomLeftButton: UIButton!
+    @IBOutlet weak var firstLayoutSelected: UIButton!
+    @IBOutlet weak var secondLayoutSelected: UIButton!
+    @IBOutlet weak var thirdLayoutSelected: UIButton!
 
-    @IBAction func thirdLayout(_ sender: UIButton) {
+    @IBAction func firstLayout(_ sender: Any) {
+        topRightButton.isHidden = true
+        bottomRightButton.isHidden = false
+        firstLayoutSelected.setImage(image, for: .highlighted)
+
+    }
+    @IBAction func secondLayout(_ sender: Any) {
+        topRightButton.isHidden = false
+        bottomRightButton.isHidden = true
+        secondLayoutSelected.setImage(image, for: .highlighted)
+
+    }
+    @IBAction func thirdLayout(_ sender: Any) {
+        topRightButton.isHidden = false
+        bottomRightButton.isHidden = false
+        thirdLayoutSelected.setImage(image, for: .highlighted)
         
     }
 
+    //MARK: GridView buttons actions
+
+    
+
 }
+
+
 
